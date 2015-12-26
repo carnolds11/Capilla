33e65e4b9378f894dc370f7992e6cc91243bca2b
Cristián Arnolds <carnolds11@gmail.com>
2015-12-26 01:04:57 -0300

    ultimos cambios

--- app/idea/scripts/IndexController.js
+++ app/idea/scripts/IndexController.js
@@ -5,6 +5,11 @@ angular
     $scope.showSpinner = true;
 
     $scope.ideas = JSON.parse(window.localStorage.getItem('jIdea'));
+    $scope.sub_frases = [];
+
+    for(var i = 0; i < $scope.ideas.length; i++){
+        $scope.sub_frases[i] = $scope.ideas[i].tip.split("+");
+    }
     $scope.showSpinner = false;
 
   });
